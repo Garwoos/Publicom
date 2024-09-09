f2d19e5ed90d715c514b9776e184233038078a61
Garwoos <saminochko@gmail.com>
2024-09-09 16:52:50 +0200

    aaaaaaaa 2

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <html<!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -13,6 +17,29 @@
             echo date('Y-m-d H:i:s');
         ?>
     </p>
-</body>
+<h1>Page d'inscription</h1>
+<form action="index.php" method="post">
+    <label for="username">Nom d'utilisateur:</label><br>
+    <input type="text" id="username" name="username" required><br><br>
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email" required><br><br>
+    <label for="password">Mot de passe:</label><br>
+    <input type="password" id="password" name="password" required><br><br>
+    <input type="submit" value="S'inscrire">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+
+    // Ici, vous pouvez ajouter du code pour enregistrer les données dans une base de données
+
+    echo "<h2>Inscription réussie!</h2>";
+    echo "<p>Nom d'utilisateur: $username</p>";
+    echo "<p>Email: $email</p>";
+}
+?>
 </body>
 </html>
