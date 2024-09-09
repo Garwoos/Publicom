c3cadcb23af223739c68e66aeaf175a2cb1b2d23
Garwoos <saminochko@gmail.com>
2024-09-09 16:40:59 +0200

    aaaa

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-php 
+<html<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Page PHP</title>
+</head>
+<body>
+    <h1>Bienvenue sur ma page PHP</h1>
+    <p>Ceci est un paragraphe de démonstration.</p>
+    <p>La date et l'heure actuelles sont : 
+        <?php
+            echo date('Y-m-d H:i:s');
+        ?>
+    </p>
+</body>
+</body>
+</html>
